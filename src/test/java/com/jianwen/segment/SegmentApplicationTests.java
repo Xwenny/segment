bc559649bf05cb1ba7a7896806d34db3f5b1d5bb
wenny <leewenny@163.com>
2018-06-03 21:16:11 +0800

    初始化

---
+++ src/test/java/com/jianwen/segment/SegmentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jianwen.segment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SegmentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
